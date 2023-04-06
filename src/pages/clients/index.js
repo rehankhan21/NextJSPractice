6d28ea76b71c0c3eeaa5289fdc715b2ec41ac824
rehankhan21 <narutop224@gmail.com>
2023-04-05 23:59:23 -0400

    dynamic use of next Link

--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "max", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The ClientsPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import styles from "../styles/Home.module.css";
 
+// this makes it so we dont send a new request to the backend when we want to link to pages
+// we remain in the same react app.
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>hello world</h1>
+      <ul>
+        <li>
+          <Link replace href="/portfolio">
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
