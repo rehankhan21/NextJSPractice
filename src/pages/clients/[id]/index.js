a4be84b250f902872750419327f13533f1e8d1b2
rehankhan21 <narutop224@gmail.com>
2023-04-06 00:04:59 -0400

    nagvigating programatically

--- src/pages/clients/[id]/index.js
+++ src/pages/clients/[id]/index.js
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage(params) {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The ClientProjectsPage</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
