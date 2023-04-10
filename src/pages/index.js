cf2649aaff4fb59cd1bcb871bc5204241fbf8891
rehankhan21 <narutop224@gmail.com>
2023-04-10 00:40:59 -0400

    pre rendered page using getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import styles from "../styles/Home.module.css";
 // we remain in the same react app.
 import Link from "next/link";
 
-export default function Home() {
+export default function Home(props) {
+  // now we can expect to get a products key from our props
+  const { products } = props;
+
   return (
     <div>
       <h1>hello world</h1>
@@ -18,6 +21,26 @@ export default function Home() {
           <Link href="/clients">Clients</Link>
         </li>
       </ul>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+// executes this func first to get the data for the above component.
+// must always return an object with a props key
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        {
+          id: "p1",
+          title: "Product 1",
+        },
+      ],
+    },
+  };
 }
