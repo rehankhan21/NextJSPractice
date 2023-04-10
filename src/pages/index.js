8f3add0ae3ba2dcf8f19e0c367639db18b04dcd2
rehankhan21 <narutop224@gmail.com>
2023-04-10 00:48:57 -0400

    used fs and path to fetch data from a seperate file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,8 @@
-import styles from "../styles/Home.module.css";
+// normally this import doesnt work on the client side
+// but if used inside getStaticProps, next knows you are using it for server side coding
+// nextjs specific imports
+import fs from "fs/promises";
+import path from "path";
 
 // this makes it so we dont send a new request to the backend when we want to link to pages
 // we remain in the same react app.
@@ -33,14 +37,16 @@ export default function Home(props) {
 // executes this func first to get the data for the above component.
 // must always return an object with a props key
 export async function getStaticProps() {
+  // this now returns a promise with the data
+  // process.cwd gives you the current working directory
+  // process.cwd is the working directory then we add arguments to dive deeper into folders
+  const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filepath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [
-        {
-          id: "p1",
-          title: "Product 1",
-        },
-      ],
+      products: data.products,
     },
   };
 }
